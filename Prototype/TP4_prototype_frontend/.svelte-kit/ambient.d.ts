7ea20e455f5088746d5d4afc7f3c11aaffc6c566
Mirko Brlek <6209489@cegepmontpetit.ca>
2026-04-08 10:47:19 -0400

    modifs pour prototype minimal

--- Prototype/TP4_prototype_frontend/.svelte-kit/ambient.d.ts
+++ Prototype/TP4_prototype_frontend/.svelte-kit/ambient.d.ts
@@ -51,7 +51,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_5408_1592913036: string;
+	export const EFC_10476_1592913036: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
@@ -222,7 +222,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_5408_1592913036: string;
+		EFC_10476_1592913036: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
